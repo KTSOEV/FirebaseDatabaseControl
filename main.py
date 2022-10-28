4d16ec8f6a2e353b4566848a8e822f6936c4eb1b
Oleg Ktsoev <olegmernin@gmail.com>
2022-10-28 12:30:47 +0300

    beauty code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter.tix import InputOnly
 import click
 import os
 import json
@@ -14,66 +13,81 @@ else:
     clear_command = 'clear'
 os.system(clear_command)
 print(Fore.GREEN)
-database_url = 'https://example.firebaseio.com/'
-database_keys = '.json?auth=<your_token>'
+database_url = 'https://<database>.firebaseio.com/'
+database_keys = '.json?auth=<token>'
 database_json = requests.get(database_url+database_keys).text
 database_dict = json.loads(database_json)
 choise = ''
+parent = ''
+child = ''
+
+def RemoveParent():
+    if(click.confirm(f'Are you sure you want to remove parent "{parent}"?', default=False)):
+        requests.delete(f'{database_url}{parent}/{database_keys}')
+        print('Removed')
+        input()
+def CreateChild():
+    print('Enter child name or path and value \nAttention! If you are creating a child of string type, then put ". For example: "Ktsoev"')
+    newChild = input('Name> ')
+    childValue = input('Value> ')
+    print(requests.put(f'{database_url}{parent}/{newChild}/{database_keys}', data=childValue))
+    input()
+def ChildOprions():
+    print('Child options')
+    print('1. Remove')
+    print('2. Edit')
+    choise = input('>')
+    if(choise == '1'):
+        if(click.confirm(f'Are you sure you want to remove child "{child}"?', default=False)):
+            requests.delete(f'{database_url}{parent}/{child}/{database_keys}')
+            print('Removed')
+            input()
+    elif(choise == '2'):
+        print('Enter new name or path for child')
+        new_child_name = input('>')
+        old_child_value = requests.get(f'{database_url}{parent}/{child}/{database_keys}').text
+        requests.delete(f'{database_url}{parent}/{child}/{database_keys}')
+        requests.put(f'{database_url}{parent}/{new_child_name}/{database_keys}', data=old_child_value)
+    else:
+        print('ERROR: Invalid option')
+        input()
+def Childs():
+    print(database_dict[parent])
+    child = input('Select child: ')
+    if(requests.get(f'{database_url}{parent}/{child}/{database_keys}').text != 'null'):
+        ChildOprions()
+    else:
+        print('ERROR: Child does not exist')
+        input()
+def EditParent():
+    print('Enter new name or path for parent')
+    new_parent_name = input('>')
+    old_parent_value = requests.get(f'{database_url}{parent}/{database_keys}').text
+    requests.delete(f'{database_url}{parent}/{database_keys}')
+    requests.put(f'{database_url}{new_parent_name}/{database_keys}', data=old_parent_value)
+def ParentSettings():
+    print('Parent options')
+    print('1. Remove')
+    print('2. Create new child')
+    print('3. Childs')
+    print('4. Edit')
+    choise = input('>')
+    if(choise == '1'):
+        RemoveParent()
+    elif(choise == '2'):
+        CreateChild()
+    elif(choise == '3'):
+        Childs()
+    elif(choise == '4'):
+        EditParent()
+    else:
+        print('ERROR: Invalid option')
 def Struct():
     print(database_dict.keys())
     print('Select Parent')
     parent = input("Parent> ")
     if(requests.get(f'{database_url}{parent}/{database_keys}').text != 'null'):
-        print('Parent options')
-        print('1. Remove')
-        print('2. Create new child')
-        print('3. Childs')
-        print('4. Edit')
-        choise = input('>')
-        if(choise == '1'):
-            if(click.confirm(f'Are you sure you want to remove parent "{parent}"?', default=False)):
-                requests.delete(f'{database_url}{parent}/{database_keys}')
-                print('Removed')
-                input()
-        elif(choise == '2'):
-            print('Enter child name or path and value \nAttention! If you are creating a child of string type, then put ". For example: "Ktsoev"')
-            newChild = input('Name> ')
-            childValue = input('Value> ')
-            print(requests.put(f'{database_url}{parent}/{newChild}/{database_keys}', data=childValue))
-            input()
-        elif(choise == '3'):
-            print(database_dict[parent])
-            child = input('Select child: ')
-            if(requests.get(f'{database_url}{parent}/{child}/{database_keys}').text != 'null'):
-                print('Child options')
-                print('1. Remove')
-                print('2. Edit')
-                choise = input('>')
-                if(choise == '1'):
-                    if(click.confirm(f'Are you sure you want to remove child "{child}"?', default=False)):
-                        requests.delete(f'{database_url}{parent}/{child}/{database_keys}')
-                        print('Removed')
-                        input()
-                elif(choise == '2'):
-                    print('Enter new name or path for child')
-                    new_child_name = input('>')
-                    old_child_value = requests.get(f'{database_url}{parent}/{child}/{database_keys}').text
-                    requests.delete(f'{database_url}{parent}/{child}/{database_keys}')
-                    requests.put(f'{database_url}{parent}/{new_child_name}/{database_keys}', data=old_child_value)
-                else:
-                    print('ERROR: Invalid option')
-                    input()
-            else:
-                print('ERROR: Child does not exist')
-                input()
-        elif(choise == '4'):
-            print('Enter new name or path for parent')
-            new_parent_name = input('>')
-            old_parent_value = requests.get(f'{database_url}{parent}/{database_keys}').text
-            requests.delete(f'{database_url}{parent}/{database_keys}')
-            requests.put(f'{database_url}{new_parent_name}/{database_keys}', data=old_parent_value)
-        else:
-            print('ERROR: Invalid option')
+        ParentSettings()
     else:
         print('ERROR: Parent does not exist')
         input()
